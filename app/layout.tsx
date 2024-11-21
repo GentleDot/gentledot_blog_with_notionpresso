8b90501d0b890c2b92332c01b3ed1ba1e8a46c17
gentledot <gentledot.wp@gmail.com>
2024-11-21 21:17:21 +0900

    feat(blog_project): layout 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
 import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from 'components/nav'
+import type {Metadata} from 'next'
+import {GeistSans} from 'geist/font/sans'
+import {GeistMono} from 'geist/font/mono'
+import {Navbar} from 'components/nav'
 import Footer from 'components/footer'
-import { baseUrl } from './sitemap'
-import { ThemeProvider } from 'next-themes'
+import {baseUrl} from './sitemap'
+import {ThemeProvider} from 'next-themes'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Notionpresso Portfolio Starter',
-    template: '%s | Notionpresso Portfolio Starter',
+    default: 'Gentledot Blog',
+    template: '%s | Gentledot Blog',
   },
-  description: 'This is my my portfloio',
+  description: '생각, 지식 및 경험을 기록하고 공유하기 위한 작은 공간입니다.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Gentledot Blog',
+    description: '생각, 지식 및 경험을 기록하고 공유하기 위한 작은 공간입니다.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Gentledot',
     locale: 'en_US',
     type: 'website',
     images: [
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
         url: '/profile.png',
         width: 800,
         height: 600,
-        alt: '프로필 이미지',
+        alt: 'gentledot logo',
       }
     ],
   },
@@ -55,9 +55,9 @@ const cx = (...classes) => classes.filter(Boolean).join(' ')
 
 export default function RootLayout({
   children,
-}: {
+                                   }: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html
       lang="en"
