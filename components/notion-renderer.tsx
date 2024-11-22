6565c79fb66bbba2dbe67a833676ee401ba0f3e3
gentledot <gentledot.wp@gmail.com>
2024-11-22 22:15:18 +0900

    feat(blog_project): 결론은 page 를 생성해야만 사용할 수 있다. 데이터베이스 페이지는 사용할 수 없다.

--- components/notion-renderer.tsx
+++ components/notion-renderer.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { Notion } from "@notionpresso/react";
+import {Notion} from "@notionpresso/react";
+import type {Post} from "content/posts";
 
-export default function NotionRenderer({ post }: { post: { title: string, content: { blocks: any[] }, image?: string } }) {
+export default function NotionRenderer({post}: { post: Post }) {
   return (
     <Notion>
       <Notion.Cover src={post.image} />
--- content/posts.ts
+++ content/posts.ts
@@ -1,32 +1,24 @@
-import post1 from '../notion-data/126ce18c-fd83-8023-9ad1-d0e1809b21c3.json'
-import post2 from '../notion-data/127ce18c-fd83-805c-bebd-d6772e18bf02.json'
+import newPost from '../notion-data/1467ff6c-518b-802c-a51d-df8e53643bab.json'
 
 const posts = [
-  {
-    title: "Maximizing the Benefits of BFF Pattern in API Design",
-    slug: "api-design-in-bff",
-    content: post1,
-    date: "2023-10-22",
-    description: "Exploring the shift from domain-centric to data-centric API endpoints after introducing BFF. This article introduces BFF and DDD concepts, then proposes API endpoint design strategies in the context of the BFF pattern.",
-    image: undefined
-  },
-  {
-    title: "Naver Search Bar UX: Analysis and Implementation Guide",
-    slug: "naver-search-bar-ux",
-    content: post2,
-    date: "2023-10-23",
-    description: "Explore the UX analysis of Naver's search bar and strategies for replicating its features. The post examines the UX triggers, selection of search results, and the refreshing of result lists. It also compares coding approaches for implementing search bar functionalities, aiming for a concise and effective execution.",
-    image: undefined
-  }
+    {
+        title: "test",
+        slug: "portfolio",
+        content: newPost,
+        date: "2024-11-22",
+        description: "about gentledot",
+        image: undefined
+    }
+
 ] as Post[];
 
 export default posts;
 
 export type Post = {
-  title: string;
-  slug: string;
-  content: { blocks: any[] };
-  date: string;
-  description: string;
-  image?: string;
+    title: string;
+    slug: string;
+    content: { blocks: any[] };
+    date: string;
+    description: string;
+    image?: string;
 };
